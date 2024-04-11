67b71e98ea1634a311d6fb7697e3f37b7c2a64c7
SeokHui Jo <suchie777@gmail.com>
2024-04-11 17:56:55 +0900

    프로젝트 기본세팅 및 라이브러리추가

---
+++ GraphcisStudy/Types.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "DirectXMath.h"
+
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+
+using uin8 = unsigned __int8;
+using uin16 = unsigned __int16;
+using uin32 = unsigned __int32;
+using uin64 = unsigned __int64;
+
+using Vec2 = DirectX::XMFLOAT2;
+using Vec3 = DirectX::XMFLOAT3;
+using Vec4 = DirectX::XMFLOAT4;
+using Color = DirectX::XMFLOAT4;
+
---
+++ GraphcisStudy/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Types.h"
+#include "Struct.h"
+#include "Values.h"
+
+//STL
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+
+//WIN
+#include <Windows.h>
+#include <assert.h>
+
+//DX
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+
+
